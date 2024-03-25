e32210430bc28a9382dcf2ae91a603af27682545
Javier <javier.penaperez08@gmail.com>
2024-03-25 17:57:45 -0400

    pass 6th stage

--- app/server.go
+++ app/server.go
@@ -12,22 +12,8 @@ const (
 	NOT_FOUND   = "HTTP/1.1 404 Not Found"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	conn, err := l.Accept()
+func handleConn(conn net.Conn) {
 	defer conn.Close()
-
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
 	buffer := make([]byte, 1024)
 	r_size, err := conn.Read(buffer)
@@ -66,4 +52,25 @@ func main() {
 		fmt.Println("Failed to write to connection")
 		os.Exit(1)
 	}
+}
+
+func main() {
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for i := 0; i < 10; i++ {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConn(conn)
+	}
+
 }
